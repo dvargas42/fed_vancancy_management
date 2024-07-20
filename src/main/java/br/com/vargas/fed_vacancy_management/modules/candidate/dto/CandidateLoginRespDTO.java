49add1958261f8ce68c0dfcfd17cbac569933eb3
dvargas42 <dvargas.eng@gmail.com>
2024-07-19 23:50:10 -0300

    feat: :tada: initial commit

---
+++ src/main/java/br/com/vargas/fed_vacancy_management/modules/candidate/dto/CandidateLoginRespDTO.java
@@ -0,0 +1,14 @@
+package br.com.vargas.fed_vacancy_management.modules.candidate.dto;
+
+public class CandidateLoginRespDTO {
+    
+    private String access_token;
+
+    public String getAccess_tokent() {
+        return access_token;
+    }
+
+    public void setAccess_token(String access_token) {
+        this.access_token = access_token;
+    }
+}
