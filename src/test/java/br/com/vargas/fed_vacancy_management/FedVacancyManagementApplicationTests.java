aac58ac9a80d9153f873d3c81a6387ebeec8438c
dvargas42 <dvargas.eng@gmail.com>
2024-07-19 23:50:48 -0300

    feat: :tada: initial commit

---
+++ src/test/java/br/com/vargas/fed_vacancy_management/FedVacancyManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vargas.fed_vacancy_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FedVacancyManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
