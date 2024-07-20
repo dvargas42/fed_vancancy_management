7b8bfe6edf3fda0d50c4a3c53c034291d9b44f88
dvargas42 <dvargas.eng@gmail.com>
2024-07-19 23:49:59 -0300

    feat: :tada: initial commit

---
+++ src/main/java/br/com/vargas/fed_vacancy_management/FedVacancyManagementApplication.java
@@ -0,0 +1,13 @@
+package br.com.vargas.fed_vacancy_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FedVacancyManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FedVacancyManagementApplication.class, args);
+	}
+
+}
