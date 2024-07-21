6922290fb8e335cb197168cf990b02c1c4e014bf
dvargas42 <dvargas.eng@gmail.com>
2024-07-20 21:24:17 -0300

    feat: :sparkles: create profile method in CandidateController

--- src/main/java/br/com/vargas/fed_vacancy_management/modules/candidate/controller/CandidateController.java
+++ src/main/java/br/com/vargas/fed_vacancy_management/modules/candidate/controller/CandidateController.java
@@ -1,6 +1,12 @@
 package br.com.vargas.fed_vacancy_management.modules.candidate.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,6 +14,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.vargas.fed_vacancy_management.modules.candidate.service.CandidateService;
+import br.com.vargas.fed_vacancy_management.modules.candidate.service.ProfileCandidateService;
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/candidate")
@@ -15,23 +23,45 @@ public class CandidateController {
 
     @Autowired
     private CandidateService candidateService;
+
+    @Autowired
+    private ProfileCandidateService profileCandidateService;
     
     @GetMapping("/login")
-    public String login() {
+    public String login() { 
         return "/candidate/login";
     }
 
     @PostMapping("/signIn")
-    public String signIn(RedirectAttributes redirectAttributes,String username, String password) {
+    public String signIn(RedirectAttributes redirectAttributes, HttpSession session, String username, String password) {
         try {
             var token = candidateService.login(username, password);
+            var grants = token.getRoles().stream().map(role -> new SimpleGrantedAuthority("ROLE_" + role.toString().toUpperCase())).toList();
 
-            return "/candidate/profile";
+            UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(null, null, grants);
+            auth.setDetails(token.getAccess_token());
+
+            SecurityContextHolder.getContext().setAuthentication(auth);
+            SecurityContext securityContext = SecurityContextHolder.getContext();
+            session.setAttribute("SPRING_SECURITY_CONTEXT", securityContext);
+            session.setAttribute("token", token);
+
+
+            return "redirect:/candidate/profile";
 
         } catch (Exception e) {
             redirectAttributes.addFlashAttribute("error_message", "Usuario/Senha incorretos");
             return "redirect:/candidate/login";
         }
-    }   
+    }
+
+    @GetMapping("/profile")
+    @PreAuthorize("hasRole('CANDIDATE')")
+    public String profile() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        var result = this.profileCandidateService.execute(authentication.getDetails().toString());
+
+        return "/candidate/profile";
+    }               
 }
     
