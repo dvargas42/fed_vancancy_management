9020f2f0f229cd7d54dd78574f25640358b8b60c
dvargas42 <dvargas.eng@gmail.com>
2024-07-20 21:27:00 -0300

    refactor: :recycle: change CandidateLoginRespDTO

--- src/main/java/br/com/vargas/fed_vacancy_management/modules/candidate/dto/CandidateLoginRespDTO.java
+++ src/main/java/br/com/vargas/fed_vacancy_management/modules/candidate/dto/CandidateLoginRespDTO.java
@@ -1,14 +1,13 @@
 package br.com.vargas.fed_vacancy_management.modules.candidate.dto;
 
+import java.util.List;
+
+import lombok.Data;
+
+@Data
 public class CandidateLoginRespDTO {
     
     private String access_token;
-
-    public String getAccess_tokent() {
-        return access_token;
-    }
-
-    public void setAccess_token(String access_token) {
-        this.access_token = access_token;
-    }
+    private List<String> roles;
+    private Long expires_in;
 }
